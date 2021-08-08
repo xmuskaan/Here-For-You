ecb4f58740b4070d576139218bf364a6aa2ed084
xmuskaan <muskaan13r@gmail.com>
2021-08-08 14:51:25 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Home from './Pages/Home';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi World!</h1>
+      <Navbar/>
+
+      <Router>
+        <Route exact path = '/' >
+          <Home/>
+        </Route>
+        <Route exact path='/login'>
+          <Login/>
+        </Route>
+        <Route exact path ='/register'>
+          <Register/>
+        </Route>
+      </Router>
+
     </div>
+   
+    
   );
 }
 
